59c53eac1eb70b7fb255b828b20d7663843996ad
kieranji <kieran.ji.zx@gmail.com>
2026-08-06 16:21:35 +0100

    Implement unique permutations algorithm

---
+++ Medium/47. Permutations II/0047-permutations-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) 
+    {
+        vector<vector<int>> output;
+        sort(nums.begin(), nums.end());
+        output.push_back(nums);
+        while(true)
+        {
+            int i=nums.size()-1;
+            int j=i;
+            while(i>0&&nums[i-1]>=nums[i])
+            {
+                i--;
+            }
+            if(i==0)
+            {
+                break;
+            }
+            while(nums[j]<=nums[i-1])
+            {
+                j--;
+            }
+            swap(nums[i-1],nums[j]);
+            reverse(nums.begin()+i,nums.end());
+            output.push_back(nums);
+        }
+        return output;    
+    }
+};
