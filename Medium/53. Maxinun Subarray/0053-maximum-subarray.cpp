820898cf0bda20041ccf22e0e6d8b064dfd12c5c
kieranji <kieran.ji.zx@gmail.com>
2026-08-10 21:27:19 +0100

    Implement maximum subarray solution in C++

---
+++ Medium/53. Maxinun Subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int current=nums[0];
+        int output=nums[0];
+
+        for(int i=1;i<nums.size();i++)
+        {
+            current=max(nums[i],current+nums[i]);
+            output=max(output,current);
+        }
+        return output;
+    }
+};
