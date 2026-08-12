ffe645d7eff2502030994808af496bf25f39f240
kieranji <kieran.ji.zx@gmail.com>
2026-08-12 22:00:00 +0100

    Solve LeetCode 55 Jump Game

---
+++ Medium/55. Jump Game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int far=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i>far)
+            {
+                return false;
+            }
+            far=max(far,nums[i]+i);
+        }
+        return true;
+    }
+};
