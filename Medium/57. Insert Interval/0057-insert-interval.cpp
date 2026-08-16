c9d0e0e12556441a26c985e345a45d14f7727665
kieranji <kieran.ji.zx@gmail.com>
2026-08-16 23:42:01 +0100

    Implement insert interval function in C++

---
+++ Medium/57. Insert Interval/0057-insert-interval.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+    {
+        intervals.push_back(newInterval);
+
+        vector<vector<int>> output;
+        sort(intervals.begin(),intervals.end());
+        int maximum=intervals[0][1];
+        int minimum=intervals[0][0];
+        for(int i=1;i<intervals.size();i++)
+        {
+            int min2=intervals[i][0];
+            int max2=intervals[i][1];
+            if(maximum>=min2)
+            {
+                maximum=max(maximum,max2);
+            }
+            else
+            {
+                output.push_back({minimum,maximum});
+                minimum=min2;
+                maximum=max2;
+            }
+        }
+        output.push_back({minimum, maximum});
+        return output;
+    }
+};
